6e69b9f80d5e615dafcae90a0beab780cdd94952
yskoh <koh.yesl@gmail.com>
2015-09-02 23:46:13 +0900

    worked on falling rain, moving player

--- fallingRain0.js
+++ fallingRain0.js
@@ -1,42 +1,57 @@
 var AvoidRainGame = {};
 var canvas= document.querySelector('.canvas');
 var ctx = canvas.getContext('2d');
+var gameStatus = true;
 
 //rain
-AvoidRainGame.Rain = (function(){
-	var color = '#F0E3FF';
-	var rainPosX = 0;
-	var rainPosY = 5;
+AvoidRainGame.RainArray =[];
+
+setInterval(function(){
+	console.log("in");
+	for(var i=0; i< 10;i++){
+		AvoidRainGame.RainArray.push(new AvoidRainGame.Rain());
+	} 
+},1000);
+
+AvoidRainGame.Rain = function(){
+	this.rainPosX = this.setRainPosX();
+	this.rainPosY = this.setRainPosY();
+}
+
+AvoidRainGame.Rain.prototype.setRainPosX = function(){
+	return rand(0, canvas.width);
+};
+
+AvoidRainGame.Rain.prototype.setRainPosY = function(){
+	return rand(-500, 0);
+};
+
+AvoidRainGame.Rain.prototype.isFalling = function(){
+	//array에 있는 비를 하나씩 꺼내서 각각 떨어뜨려준다.
 	var fallAmount =3;
+	if(this.rainPosY < canvas.height){
+		//set falling = true;
+		return this.rainPosY = this.rainPosY + fallAmount;
+	}else{
+		//add point
+		//delete rain
+		//set falling = false;
+		console.log("end");
+		return;
+	}
+};
+AvoidRainGame.Rain.prototype.draw = function(){
 	var rainRadius = 5;
+	var color = '#F0E3FF';
 	var startAngle = 0;
 	var endAngle = 2* Math.PI;
 
-	return{
-		setRainPosX: function(){
-			return rand(0, canvas.width);
-		},
-		isFalling: function(){
-			if(rainPosY < canvas.height){
-				return rainPosY = rainPosY + fallAmount;
-			}else{
-				//add point
-				//delete rain
-				console.log("end");
-				return;
-			}
-		},
-		draw: function(){
-			// var context = AvoidRainGame.Map.getContext();
-			this.setRainPosX();
-            ctx.beginPath();
-            ctx.arc(this.setRainPosX(), rainPosY, rainRadius, startAngle, endAngle);
-            ctx.fillStyle = color;
-            ctx.fill();
-            ctx.closePath();
-		},
-	}
-})();
+    ctx.beginPath();
+    ctx.arc(this.rainPosX, this.rainPosY, rainRadius, startAngle, endAngle);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+};
 
 //character
 AvoidRainGame.Player = (function(){
@@ -67,8 +82,11 @@ AvoidRainGame.Player = (function(){
 
 AvoidRainGame.drawAll = function(){
 	ctx.clearRect(0,0,canvas.width, canvas.height);
-	AvoidRainGame.Rain.isFalling();
-	AvoidRainGame.Rain.draw();
+	//put rain into array and draw all of them
+	for(var i =0; i<AvoidRainGame.RainArray.length; i++){
+		AvoidRainGame.RainArray[i].isFalling();
+		AvoidRainGame.RainArray[i].draw();
+	}
 	AvoidRainGame.Player.draw();
 	if(gameStatus == true){
 		AvoidRainGame.requestAnimationFrame = window.requestAnimationFrame(AvoidRainGame.drawAll);
@@ -81,6 +99,9 @@ function rand(min, max){
 	return (Math.random()*(max -min) + min);
 };
 
+for(var i=0; i< 30;i++){
+		AvoidRainGame.RainArray.push(new AvoidRainGame.Rain());
+	} 
 //added event to move with keys
 document.addEventListener('keydown', stepLeftOrRight);
 
